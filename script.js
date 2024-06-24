41cc7aaad3edf2b24bd31c75776cc1374d4a7699
envisational <theman97m@gmail.com>
2024-06-24 17:32:06 +0300

    Logic implemented (need to add music and check JS script to make sure)

--- script.js
+++ script.js
@@ -76,6 +76,13 @@ audio.addEventListener("ended", () => {
 
     if (nextSongExists) {
       playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;
+      pauseSong();
+      setPlayerDisplay();
+      highlightCurrentSong();
+      setPlayButtonAccessibleText();
     }
 })
 
