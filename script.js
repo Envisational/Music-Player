d1ddb25d1178c577c8b1f4a4f8ce98932fba2bc1
envisational <theman97m@gmail.com>
2024-06-24 15:55:10 +0300

    text content property set in setPlayerDisplay function

--- script.js
+++ script.js
@@ -129,6 +129,16 @@ const playSong = (id) => {
       }
   };
 
+  // Display current song title and artist
+  const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+    
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+  }
   const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
     const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
